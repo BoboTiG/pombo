59d3e6842a9de80998d01615b8b43904dd4ef759
BoboTiG <bobotig@gmail.com>
2013-10-18 19:01:31 +0200

    GNU/Linux: test if /etc/crontab exists before operate

--- gnu-linux/uninstall.sh
+++ gnu-linux/uninstall.sh
@@ -4,29 +4,29 @@
 #
 
 if [ $(id -ru) -ne 0 ]; then
-	echo "! You need to have root rights !"
-	su root -c $0
-	exit 0
+    echo "! You need to have root rights !"
+    su root -c $0
+    exit 0
 fi
 
 echo "\nUninstalling (verbose) ..."
 rm -fv /etc/pombo.conf
 [ -f /var/local/pombo ] && rm -fv /var/local/pombo
 if [ -f /usr/local/bin/pombo.py ]; then
-	# Retro-compatibility (version <= 0.0.9)
-	rm -fv /usr/local/bin/pombo.py
-	echo "« sed -i '\/usr\/local\/bin\/pombo.py/d' /etc/crontab »"
-	sed -i '\/usr\/local\/bin\/pombo.py/d' /etc/crontab
+    # Retro-compatibility (version <= 0.0.9)
+    rm -fv /usr/local/bin/pombo.py
+    echo "« sed -i '\/usr\/local\/bin\/pombo.py/d' /etc/crontab »"
+    sed -i '\/usr\/local\/bin\/pombo.py/d' /etc/crontab
 else
-	[ -f /usr/local/bin/pombo ] && rm -fv /usr/local/bin/pombo
-	[ -f /usr/local/sbin/pombo ] && rm -fv /usr/local/sbin/pombo
-	if [ $(grep -c "/usr/local/bin/pombo" /etc/crontab) != 0 ] ; then
-		# Retro-compatibility (version = 0.0.9)
-		echo "« sed -i '/usr/local/bin/pombo/d' /etc/crontab »"
-		sed -i '\/usr\/local\/bin\/pombo/d' /etc/crontab
-	else
-		rm -fv /etc/cron.d/pombo
-	fi
+    [ -f /usr/local/bin/pombo ] && rm -fv /usr/local/bin/pombo
+    [ -f /usr/local/sbin/pombo ] && rm -fv /usr/local/sbin/pombo
+    [ -f /etc/crontab ] && if [ $(grep -c "/usr/local/bin/pombo" /etc/crontab) != 0 ] ; then
+        # Retro-compatibility (version = 0.0.9)
+        echo "« sed -i '/usr/local/bin/pombo/d' /etc/crontab »"
+        sed -i '\/usr\/local\/bin\/pombo/d' /etc/crontab
+    else
+        rm -fv /etc/cron.d/pombo
+    fi
 fi
 echo "Done."
 
@@ -34,7 +34,7 @@ cat <<EOM
 
 Thank you to have used Pombo!
 If you have critics, suggestions, advices or ideas:
-	bobotig (at) gmail (dot) com
+    bobotig (at) gmail (dot) com
 
 EOM
 
