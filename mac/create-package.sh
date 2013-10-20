a0bcbbdecfc1d291c2cb448ced0512773e5e1080
BoboTiG <bobotig@gmail.com>
2013-10-20 15:27:32 +0200

    [M] Simpler method to create package

--- mac/create-package.sh
+++ mac/create-package.sh
@@ -1,12 +1,7 @@
-#!/bin/bash -e
+#!/bin/sh -e
 #
 # Package creation for Pombo (Mac OSX).
 #
-
-[ -z "$1" ] && version=$(cat ../VERSION) || version="$1"
-output="pombo-$version.dmg"
-src=~/Desktop/pombo
-tmp="$src/tmp"
 
 files=" check-imports.py \
         install.sh \
@@ -22,16 +17,6 @@ files=" check-imports.py \
         ../VERSION \
 "
 
-[ -f $output ] && rm -rf $output
-[ -d "$tmp" ] && rm -rf "$tmp"
-mkdir "$tmp"
-mkdir "$tmp/data"
-cp $files "$tmp/data/"
-cp Install "$tmp/"
-
-bash create-dmg.sh --window-size 360 170 \
-    --icon-size 114 \
-    --volname "Pombo" \
-    $output \
-    "$tmp"
-rm -rf "$tmp"
+[ -z "$1" ] && version=$(cat ../VERSION) || version="$1"
+output="pombo-$version-macosx.tar.gz"
+tar -cf - $files | gzip -f >$output
