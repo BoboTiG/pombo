9b3dde0f1771abb71955e11b5f590162c9eb056c
Anard <anard@free.fr>
2018-09-13 18:12:18 +0200

    Add files via upload
    Launch Pombo on startup via root's crontab.
    Else, PC could run during 15 minutes without launching pombo.

--- gnu-linux/install.sh
+++ gnu-linux/install.sh
@@ -5,40 +5,49 @@
 
 if [ $(id -ru) -ne 0 ]; then
     echo "! You need to have root rights !"
-    exit 1
+    su root -c $0
+    exit 0
 fi
 
 inst_dir=/usr/local/bin
 [ -d ${inst_dir} ] || inst_dir=/usr/local/sbin
 
-src_dir=""
-[ -f pombo.py ] || src_dir="../"
-
 echo "\nInstalling (verbose) ..."
 [ -f /etc/pombo.conf ] && mv -fv /etc/pombo.conf /etc/pombo.conf.$(date '+%s')
-install -v ${src_dir}pombo.conf /etc
-echo "« chmod 600 /etc/pombo.conf »"
+install -v pombo.conf /etc
+echo "« chmod 600 /etc/pombo.conf »"
 chmod 600 /etc/pombo.conf
-install -v ${src_dir}pombo.py ${inst_dir}/pombo
-echo "« chmod +x ${inst_dir}/pombo »"
+install -v pombo.py ${inst_dir}/pombo
+echo "« chmod +x ${inst_dir}/pombo »"
 chmod +x ${inst_dir}/pombo
 
 if test -f /etc/crontab ; then
     # Retro-compatibility (version <= 0.0.9)
     if [ $(grep -c "/usr/local/bin/pombo" /etc/crontab) != 0 ] ; then
-        echo "« sed -i '/usr/local/bin/pombo/d' /etc/crontab »"
+        echo "« sed -i '/usr/local/bin/pombo/d' /etc/crontab »"
         sed -i '\/usr\/local\/bin\/pombo/d' /etc/crontab
     fi
 fi
+# Launch Pombo on boot
+if test -f /var/spool/cron/crontabs/root ; then
+    # Remove possible old entries
+    if [ $(grep -c "pombo" /var/spool/cron/crontabs/root) != 0 ] ; then
+        echo "« sed -i '\/pombo/d' /var/spool/cron/crontabs/root »"
+        sed -i '\/pombo/d' /var/spool/cron/crontabs/root
+    fi
+    echo "« @startup ${inst_dir}/pombo >>/var/spool/cron/crontabs/root »"
+    echo "@startup ${inst_dir}/pombo" >>/var/spool/cron/crontabs/root
+fi
+
 [ -f /etc/cron.d/pombo ] && rm -fv /etc/cron.d/pombo
-echo "« */15 * * * * root ${inst_dir}/pombo >>/etc/cron.d/pombo »"
+echo "« */15 * * * * root ${inst_dir}/pombo >>/etc/cron.d/pombo »"
 echo "*/15 * * * * root ${inst_dir}/pombo" >>/etc/cron.d/pombo
 [ -f /var/local/pombo ] && rm -fv /var/local/pombo
 echo "Done."
 
 echo "\nChecking dependancies ..."
 ok=1
-for package in python gpg ifconfig iwlist traceroute streamer; do
+for package in python gpg ifconfig iwlist traceroute scrot streamer; do
     test=$(which ${package})
     [ $? != 0 ] && echo " ! ${package} needed but not installed." && ok=0
 done
@@ -50,7 +59,6 @@ case ${ok} in
 esac
 
 cat <<EOM
-
 Thank you to use Pombo!
 Then you will need to:
     1 - to enable encryption, import your GnuPG keyID
@@ -58,6 +66,5 @@ Then you will need to:
     3 - tune variables into pombo.php
     4 - copy pombo.php to your server(s) (both PHP versions 4 & 5 supported)
 And do not forget to write somewhere in security your computer Serial Number.
-
 EOM
 exit 0
