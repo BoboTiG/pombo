57ab780abdb7c3fee9d4e6d5ca975058d296de7c
Anard <anard@free.fr>
2018-09-14 09:52:04 +0200

    Add files via upload
    Fichier anciennement repris depuis la branche master.
    Corrigé

--- gnu-linux/install.sh
+++ gnu-linux/install.sh
@@ -5,26 +5,28 @@
 
 if [ $(id -ru) -ne 0 ]; then
     echo "! You need to have root rights !"
-    su root -c $0
-    exit 0
+    exit 1
 fi
 
 inst_dir=/usr/local/bin
 [ -d ${inst_dir} ] || inst_dir=/usr/local/sbin
 
+src_dir=""
+[ -f pombo.py ] || src_dir="../"
+
 echo "\nInstalling (verbose) ..."
 [ -f /etc/pombo.conf ] && mv -fv /etc/pombo.conf /etc/pombo.conf.$(date '+%s')
-install -v pombo.conf /etc
-echo "« chmod 600 /etc/pombo.conf »"
+install -v ${src_dir}pombo.conf /etc
+echo "« chmod 600 /etc/pombo.conf »"
 chmod 600 /etc/pombo.conf
-install -v pombo.py ${inst_dir}/pombo
-echo "« chmod +x ${inst_dir}/pombo »"
+install -v ${src_dir}pombo.py ${inst_dir}/pombo
+echo "« chmod +x ${inst_dir}/pombo »"
 chmod +x ${inst_dir}/pombo
 
 if test -f /etc/crontab ; then
     # Retro-compatibility (version <= 0.0.9)
     if [ $(grep -c "/usr/local/bin/pombo" /etc/crontab) != 0 ] ; then
-        echo "« sed -i '/usr/local/bin/pombo/d' /etc/crontab »"
+        echo "« sed -i '/usr/local/bin/pombo/d' /etc/crontab »"
         sed -i '\/usr\/local\/bin\/pombo/d' /etc/crontab
     fi
 fi
@@ -40,14 +42,14 @@ if test -f /var/spool/cron/crontabs/root ; then
 fi
 
 [ -f /etc/cron.d/pombo ] && rm -fv /etc/cron.d/pombo
-echo "« */15 * * * * root ${inst_dir}/pombo >>/etc/cron.d/pombo »"
+echo "« */15 * * * * root ${inst_dir}/pombo >>/etc/cron.d/pombo »"
 echo "*/15 * * * * root ${inst_dir}/pombo" >>/etc/cron.d/pombo
 [ -f /var/local/pombo ] && rm -fv /var/local/pombo
 echo "Done."
 
 echo "\nChecking dependancies ..."
 ok=1
-for package in python gpg ifconfig iwlist traceroute scrot streamer; do
+for package in python gpg ifconfig iwlist traceroute streamer; do
     test=$(which ${package})
     [ $? != 0 ] && echo " ! ${package} needed but not installed." && ok=0
 done
@@ -59,6 +61,7 @@ case ${ok} in
 esac
 
 cat <<EOM
+
 Thank you to use Pombo!
 Then you will need to:
     1 - to enable encryption, import your GnuPG keyID
@@ -66,5 +69,6 @@ Then you will need to:
     3 - tune variables into pombo.php
     4 - copy pombo.php to your server(s) (both PHP versions 4 & 5 supported)
 And do not forget to write somewhere in security your computer Serial Number.
+
 EOM
 exit 0
